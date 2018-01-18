71c0c3050a610cab0cf3de0043d7864a7f17d7c8
Pat Gunn <pgunn01@gmail.com>
2018-01-17 23:44:34 -0500

    Fix a bug, also finish the tag list page

--- blogsql.go
+++ blogsql.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"log"
 )
 
 
@@ -10,11 +9,6 @@ import (
 func get_blogentry(dbh *sql.DB, id string) (map[string]string, map[string]string) {
 	// Given an id in the blogentry table, return everything
 	// about it needed to display it, including tags.
-	// SELECT * FROM blogentry WHERE id=$id
-	// and also
-	// SELECT id as tagid, tagname FROM tag WHERE tagid IN (
-	//	SELECT tagid FROM blogentry_tags WHERE beid=$id)
-	// TODO: Actually do the tags part
 	var mymap = make(map[string]string)
 	var tags = make(map[string]string)
 
@@ -39,20 +33,17 @@ func identify_last_n_blogentries(dbh *sql.DB, count int, include_private bool) [
 	// Returns blogentry(id) for the last up-to-$count blogentries
 	// NOTE: It can return fewer than requested if there are not that many.
 	// It will return the entries ordered so newer are earlier.
-	// SELECT id FROM blogentry WHERE hidden=$include_private
-	// 	ORDER BY zeit DESC LIMIT $count
 	var ret []string
+	var dbq *sql.Rows
 
-	dbq, err := dbh.Query("SELECT id FROM blogentry WHERE private=$1 ORDER BY zeit LIMIT $2", include_private, count)
-	if err != nil {
-		ret = append(ret, "1")
-		ret = append(ret, "2")
-		log.Print(err)
-		return ret
+	if include_private {
+		dbq, _ = dbh.Query("SELECT id FROM blogentry ORDER BY zeit LIMIT $2", include_private, count)
+	} else {
+		dbq, _ = dbh.Query("SELECT id FROM blogentry WHERE private=false ORDER BY zeit LIMIT $1", count)
 	}
 	for dbq.Next() {
 		var retval string
-		err = dbq.Scan(&retval)
+		dbq.Scan(&retval)
 		ret = append(ret, retval)
 	}
 	return ret
